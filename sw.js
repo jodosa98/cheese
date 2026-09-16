03833827bfec0235afbbc1f31aee3c553338443f
jodosa98 <tjrwnscjswp@gmail.com>
2026-09-16 19:46:08 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 오프라인 지원용 서비스 워커. 앱 파일을 캐시하고, 인터넷이 있으면 새 버전을 받아옵니다.
-const CACHE = 'cheese-v20';
+const CACHE = 'cheese-v22';
 const FILES = ['./', './index.html', './firebase-config.js', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
